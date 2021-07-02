6fd37b0b48c24540060dde6d93ec645adb7d0976
takapiro99 <55702777+takapiro99@users.noreply.github.com>
2021-07-03 01:22:16 +0900

    ルーティングをできるようにした

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,26 @@
-import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Footer from "./components/Footer";
 import Mypage from "./pages/Mypage";
+import NotFound from "./pages/NotFound";
+import SignIn from "./pages/SignIn";
+import Top from "./pages/Top";
 
 function App() {
   return (
     <div className="App">
-      <Mypage />
+      <BrowserRouter>
+        <div className="mainBody">
+          <Switch>
+            <Route exact path="/" component={Top} />
+            <Route exact path="/signin" component={SignIn} />
+            {/* TODO:  */}
+            {/* <Route exact path="/reviews" component={SignIn} /> */}
+            <Route path="/:id" component={Mypage} />
+            <Route component={NotFound} />
+          </Switch>
+        </div>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+  return (
+    <div style={{ border: "1px grey solid" }}>
+      仮のfooterです
+      <Link to="/">top</Link>
+      <span>&nbsp;</span>
+      <Link to="/signin">signin</Link>
+      <span>&nbsp;</span>
+      <Link to="/@myID">mypage</Link>
+      <span>&nbsp;</span>
+      <Link to="/404">not found</Link>
+    </div>
+  );
+};
+
+export default Footer;
