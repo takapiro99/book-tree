e74f37580c3e7a5de168daea82224d2881cceb81
Nano3013 <kuma213213@icloud.com>
2021-07-03 23:09:34 +0900

    Reviewtree　形だけ

--- frontend/src/components/review_tree.tsx
+++ frontend/src/components/review_tree.tsx
@@ -1,13 +1,19 @@
 import review_tree from "../assets/review_tree.png";
+import icon from "../assets/icon_LittleTree.png"
 
 import styles from "../styles/ReviewTree.module.scss";
 
 
 const Reviewtree = () => {
     return (
-    <div>
-        <img className={styles.review_tree}src={review_tree} alt="reviewtree" />
-    </div>
+    
+        <div className={styles.reviewtree}>
+            <img className={styles.reviewtree_pic}src={review_tree} alt="reviewtree" />
+            <p className={styles.sentence_1}>NEKOさん</p>
+            <p className={styles.sentence_2}>北大OG　＋　文系からIT　＋　猫が好き</p>
+            <div className={styles.icon_pic}><img  src={icon} alt="TwitterIcon" /></div>
+        </div>
+        
     );
 };
 
