b85e6261a6435d13fe4c3eaa6e6d8f0ba451f726
Nano3013 <kuma213213@icloud.com>
2021-07-04 00:42:42 +0900

    Co-authored-by: Saku Panda <SakuPanda39@users.noreply.github.com> Co-authored-by: Takahiro Nishino <takapiro99@users.noreply.github.com>

--- frontend/src/components/LittleTree.tsx
+++ frontend/src/components/LittleTree.tsx
@@ -1,18 +1,20 @@
 import littletree from "../assets/LittleTree_flag.png";
-import icon from "../assets/icon_LittleTree.png";
+import icon from "../assets/icon1.png";
 import styles from "../styles/LittleTree.module.scss";
 
 const LittleTree = () => {
     // 文字カウントする関数
-
+    const content = "フロントエンド"
     return (
     <div>
         <div className={styles.flag_sentence}>
             <img className={styles.littletree_pic} src={littletree} alt="littetree" />        
-            <p className={styles.flag_sentence_1}>フロントエンド</p>
+            <p className={styles.flag_sentence_1} style={{fontSize: `${content.length}px`}}>{content}</p>
             <p className={styles.flag_sentence_2}>がすごい人！</p>
+            <div className={styles.icon_pic}><img  src={icon} alt="TwitterIcon" /></div>
+
         </div>
-                <div className={styles.icon_pic}><img  src={icon} alt="TwitterIcon" /></div>
+        {/* <div className={styles.icon_pic}><img  src={icon} alt="TwitterIcon" /></div> */}
     </div>
     );
 };
