498a57d38c118d1057f74f4c5a69ff530300aad9
Saku Panda <70760422+SakuPanda39@users.noreply.github.com>
2021-07-08 06:25:23 +0900

    フォームのコードを書きたい

--- frontend/src/components/AddReview.tsx
+++ frontend/src/components/AddReview.tsx
@@ -1,6 +1,6 @@
 import styles from "../styles/ReviewForm.module.scss";
-import Books from "./Books";
-const AddReview = () => {
+import Books, { BooksProps } from "./Books";
+const AddReview = (props:BooksProps) => {
     return(
         <div className={`${styles.reviewformAddreview} ${styles.blockbtwMd}`}>
         <div className={styles.reviewformAddreviewBooks}>
@@ -9,13 +9,13 @@ const AddReview = () => {
         <div className={styles.reviewformAddreviewReview}>
         <div className={styles.reviewformAddreviewReview__reason}>
         <select>
-        <option selected　value="grapefruit">**＊を始めたい人におすすめ</option>
+        <option defaultValue="grapefruit">**を始めたい人におすすめ</option>
         <option value="lime">影響を受けたのはこの本！</option>
         <option value="coconut">難しいけどおすすめ</option>
         <option value="orange">一番</option>
         </select>
         </div> 
-        <input className={styles.reviewformAddreviewReview__review} placeholder="レビューを入力してください" />
+        <textarea className={styles.reviewformAddreviewReview__review} placeholder="レビューを入力してください" />
         </div>
     </div> 
     );
--- frontend/src/pages/review/Form.tsx
+++ frontend/src/pages/review/Form.tsx
@@ -2,9 +2,57 @@ import styles from "../../styles/ReviewForm.module.scss";
 import Books from "../../components/Books";
 import AddReview from "../../components/AddReview";
 import globalStyles from "../../styles/Global.module.scss";
+import { useState } from 'react';
+import { BooksProps } from '../../components/Books';
+import axios from 'axios'
 
+    // インターフェース
+interface IData {
+        threeWords: string[]
+        book:BooksProps
+        selectedBooks:([])
+      }
+      // 初期データ
+const initialData: IData = {
+        threeWords: [],
+        book:{
+        bookImageURL:"",
+        displayType:"bookOnly",
+        bookLink:"",
+        userID:"",
+        },
+        selectedBooks:([]),
+      }
 
 const Form = () => {
+
+    //保存するデータ
+    const [data, setData] = useState<IData>(initialData);
+    //保存しないデータ
+    const [title,setTitle] = useState('')
+    
+    const handleChangeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const value = event.target.value;
+        const name = event.target.name;
+        setData({ ...data, [name]: value });
+        console.log(data)
+    }
+    const searchBooks = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const value = event.target.value;
+        setTitle(value)
+        console.log(title)
+        //ここでAPIを用いて本の情報を取得する
+        axios
+        .get(`https://www.googleapis.com/books/v1/volumes?q=${title}`)
+        .then((res) => {
+          console.log(res.data)
+        })
+        .catch((error) => {
+          // alert('err occurred')
+          console.error(error)
+        })
+    }
+
     return <div>
     <div className={styles.yellowgreenbar}></div>
 
@@ -28,26 +76,35 @@ const Form = () => {
             <h2>3つのキーワード</h2>
             <div className={styles.reviewform3keywords__example}>例：北大2年 + カフェが好き + 漫画が好き</div>
             <div className={styles.reviewform3keywords__list}>
-            <input className={styles.reviewform3keywords__3box} /> <span className={styles.reviewform__plus}><i className="fas fa-plus"></i> </span>
-            <input className={styles.reviewform3keywords__3box} /> <span className={styles.reviewform__plus}><i className="fas fa-plus"></i></span>
-            <input className={styles.reviewform3keywords__3box} />
+            <input className={styles.reviewform3keywords__3box} name="threeWords" value={data.threeWords} onChange={handleChangeInput}/> <span className={styles.reviewform__plus}><i className="fas fa-plus"></i> </span>
+            <input className={styles.reviewform3keywords__3box} name="threeWords" value={data.threeWords} onChange={handleChangeInput}/> <span className={styles.reviewform__plus}><i className="fas fa-plus"></i></span>
+            <input className={styles.reviewform3keywords__3box} name="threeWords" value={data.threeWords} onChange={handleChangeInput}/>
             </div>
             </div>
         </div>
         <div className={`${styles.reviewformBookselect} ${styles.blockbtwMd}`}>
             <h2>本を選ぶ</h2>
             <div className={styles.reviewformBookselect__block}>
-            <input type="text" className={styles.reviewformBookselect__input} placeholder="本のタイトルを入力する" />
+            <input type="text" className={styles.reviewformBookselect__input} placeholder="本のタイトルを入力する" name="searchBookTitle" value={title} onChange={searchBooks}/>
             <div className={styles.reviewformBookselectResult}>
             {/* map */}
             <Books bookImageURL="" bookLink="" displayType="bookOnly" userID="sakusaku" />
             </div>
             </div>
         </div>
-
-        <AddReview />
-        <AddReview />
-        
+        {
+            data.selectedBooks.length && data.selectedBooks.map((book) =>{
+                return(
+                    <AddReview                             
+                    bookImageURL={data.book.bookImageURL}
+                    bookLink={data.book.bookLink}
+                    displayType={data.book.displayType}
+                    userID={data.book.userID}
+                    key={data.book.userID}
+                />
+                )
+            })
+        }
         <div className={styles.reviewformSubmit}>
         <button className={styles.submitButton}>決定</button>
         </div>
