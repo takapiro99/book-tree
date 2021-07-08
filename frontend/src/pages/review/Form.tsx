36209326439e3fa94b712120ef06a479edfedc62
Saku Panda <70760422+SakuPanda39@users.noreply.github.com>
2021-07-09 06:07:15 +0900

    フォームにuseDebounceフックを追加した

--- frontend/src/pages/review/Form.tsx
+++ frontend/src/pages/review/Form.tsx
@@ -2,34 +2,50 @@ import styles from "../../styles/ReviewForm.module.scss";
 import Books from "../../components/Books";
 import AddReview from "../../components/AddReview";
 import globalStyles from "../../styles/Global.module.scss";
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { BooksProps } from '../../components/Books';
-import axios from 'axios'
+import axios from 'axios';
+import useDebounce from '../../lib/useDebounce'
 
     // インターフェース
 interface IData {
         threeWords: string[]
-        book:BooksProps
-        selectedBooks:([])
+        books:BooksProps[]
       }
       // 初期データ
 const initialData: IData = {
         threeWords: [],
-        book:{
+        books:[{
         bookImageURL:"",
         displayType:"bookOnly",
         bookLink:"",
         userID:"",
-        },
-        selectedBooks:([]),
+        }],
       }
 
 const Form = () => {
 
     //保存するデータ
     const [data, setData] = useState<IData>(initialData);
+    //const [books, setBooks] = useState<BooksProps[]>([])
     //保存しないデータ
-    const [title,setTitle] = useState('')
+    const [title,setTitle] = useState<string>("")
+
+    const debouncedValue = useDebounce<string>(title, 400)
+
+    useEffect(() => {
+        axios
+        .get(`https://www.googleapis.com/books/v1/volumes?q=${debouncedValue}`)
+        .then((res) => {
+          console.log(res.data.items)
+          //帰ってきたデータからタイトルと画像データを取得し、
+          //data内のbooksに入れる
+        })
+        .catch((error) => {
+          // alert('err occurred')
+          console.error(error)
+        })
+    }, [debouncedValue])
     
     const handleChangeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
         const value = event.target.value;
@@ -37,21 +53,10 @@ const Form = () => {
         setData({ ...data, [name]: value });
         console.log(data)
     }
-    const searchBooks = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const value = event.target.value;
-        setTitle(value)
-        console.log(title)
-        //ここでAPIを用いて本の情報を取得する
-        axios
-        .get(`https://www.googleapis.com/books/v1/volumes?q=${title}`)
-        .then((res) => {
-          console.log(res.data)
-        })
-        .catch((error) => {
-          // alert('err occurred')
-          console.error(error)
-        })
+    const SearchBooks = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setTitle(event.target.value);
     }
+
 
     return <div>
     <div className={styles.yellowgreenbar}></div>
@@ -76,24 +81,27 @@ const Form = () => {
             <h2>3つのキーワード</h2>
             <div className={styles.reviewform3keywords__example}>例：北大2年 + カフェが好き + 漫画が好き</div>
             <div className={styles.reviewform3keywords__list}>
-            <input className={styles.reviewform3keywords__3box} name="threeWords" value={data.threeWords} onChange={handleChangeInput}/> <span className={styles.reviewform__plus}><i className="fas fa-plus"></i> </span>
-            <input className={styles.reviewform3keywords__3box} name="threeWords" value={data.threeWords} onChange={handleChangeInput}/> <span className={styles.reviewform__plus}><i className="fas fa-plus"></i></span>
-            <input className={styles.reviewform3keywords__3box} name="threeWords" value={data.threeWords} onChange={handleChangeInput}/>
+            <input className={styles.reviewform3keywords__3box} name="threeWords" value={data.threeWords[0]} onChange={handleChangeInput}/> <span className={styles.reviewform__plus}><i className="fas fa-plus"></i> </span>
+            <input className={styles.reviewform3keywords__3box} name="threeWords" value={data.threeWords[1]} onChange={handleChangeInput}/> <span className={styles.reviewform__plus}><i className="fas fa-plus"></i></span>
+            <input className={styles.reviewform3keywords__3box} name="threeWords" value={data.threeWords[2]} onChange={handleChangeInput}/>
             </div>
             </div>
         </div>
         <div className={`${styles.reviewformBookselect} ${styles.blockbtwMd}`}>
             <h2>本を選ぶ</h2>
             <div className={styles.reviewformBookselect__block}>
-            <input type="text" className={styles.reviewformBookselect__input} placeholder="本のタイトルを入力する" name="searchBookTitle" value={title} onChange={searchBooks}/>
+            <input type="text" className={styles.reviewformBookselect__input} placeholder="本のタイトルを入力する" name="searchBookTitle" value={title} onChange={SearchBooks}/>
             <div className={styles.reviewformBookselectResult}>
-            {/* map */}
-            <Books bookImageURL="" bookLink="" displayType="bookOnly" userID="sakusaku" />
-            </div>
+            { data.books.length && data.books.map((b) => {
+            return(
+            <Books bookImageURL={b.bookImageURL} bookLink="" displayType="bookOnly" userID="sakusaku" />
+            )}
+            )}
+        </div>
             </div>
         </div>
-        {
-            data.selectedBooks.length && data.selectedBooks.map((book) =>{
+        {/* {
+            data.books.length && data.selectedBooks.map((book) =>{
                 return(
                     <AddReview                             
                     bookImageURL={data.book.bookImageURL}
@@ -104,7 +112,7 @@ const Form = () => {
                 />
                 )
             })
-        }
+        } */}
         <div className={styles.reviewformSubmit}>
         <button className={styles.submitButton}>決定</button>
         </div>
