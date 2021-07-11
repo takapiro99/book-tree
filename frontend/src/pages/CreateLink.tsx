99a0ec666c3e99b5f5fc443db2a158f19b6bacea
Saku Panda <70760422+SakuPanda39@users.noreply.github.com>
2021-07-11 23:05:02 +0900

    お願いリンクページ説明以外作った

--- frontend/src/pages/CreateLink.tsx
+++ frontend/src/pages/CreateLink.tsx
@@ -1,7 +1,35 @@
 import globalStyles from '../styles/Global.module.scss'
+import styles from "../styles/createLink.module.scss"
+import Iconpic from '../components/Iconpic'
 
 const CreateLink = () => {
-    return <div>ok</div>
+    return <div className={globalStyles.wrapper}>
+
+        <div className={styles.nominateBlock}>
+            <div className={styles.nominateReasonWrapper}>
+            <h1 className={styles.nominateBlock__reason}>
+                デザインがすごいあなたに<br/>おすすめの本をとっても教えてほしーーーーーい！
+            </h1>
+            </div>
+
+            <div className={styles.nominateFromWho}>
+                <Iconpic />
+            </div>
+
+            <div　className={styles.nominateExplation}>
+                あなたがレビューを書いてくれると＊＊＊さんのBOOKTREEにあなたのレビューが実ります。
+            </div>
+        
+
+        <div className={styles.reviewSteps}>
+            <div>explation1</div>
+            <div>2</div>
+        </div>
+        <div className={styles.accontCreateFromRecom}>createここにアカウント作るを入れる</div>
+
+
+        </div>
+    </div>
 }
 
 export default CreateLink
