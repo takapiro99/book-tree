7b1030fb89b6466277f7f5ab9ff0d4634a953e3c
Saku Panda <70760422+SakuPanda39@users.noreply.github.com>
2021-07-12 19:42:53 +0900

    お願いリンク作成ページを作った

--- frontend/src/pages/ CreateLink.tsx
+++ frontend/src/pages/ CreateLink.tsx
@@ -1,14 +1,20 @@
 import globalStyles from '../styles/Global.module.scss'
 import styles from '../styles/CreateLink.module.scss'
+import BOOKTREE from "../assets/BOOKTREE.png"
 
 
 const CreateLink = () => {
     return <div className={globalStyles.wrapper}>
+        <div className={styles.createLinkWrapper}>
         <h1 className={styles.createLinkTitle}>お願いリンクを作る</h1>
         <div className={styles.createLinkBlock}>
-            <input placeholder="どんなとこがすごい？" /><span>がすごいひとにお願いする</span><button>作成</button>
+            <img src={BOOKTREE} alt='ブックツリー' className={styles.createLinkBlock__tree}/>
+            <div className={styles.createLinkBlock__input}><input className={styles.createLinkBlock__reason} placeholder="なにがすごい？" /><span>がすごいひとにお願いする</span></div>
+            <div className={styles.createButtonBlock}><button className={styles.createButtonBlock__button}>作成</button></div>
+            
+            </div>
+        <div className={styles.LinkCreated}>リンクをコピー<span className={styles.LinkCreated__share}><i className="fas fa-external-link-alt"></i></span></div>
         </div>
-        <div className={styles.LinkCreated}><span className={styles.LinkCreated__share}><i className="fas fa-external-link-alt"></i></span></div>
     </div>
 }
 
