5c68a2c2e01f267e1844164cb9a379f485561664
takapiro99 <55702777+takapiro99@users.noreply.github.com>
2021-08-24 10:21:20 +0900

    add signin

---
+++ frontend/pages/auth/signin.tsx
@@ -0,0 +1,13 @@
+import styles from './Signin.module.scss'
+
+const SignIn = () => {
+    return (
+        <div>
+            <p className={styles.title}>BOOKTREEにログインする</p>
+            <p className={styles.Twitter}>Twitterアカウントでログイン</p>
+            <p className={styles.Google}>Googleアカウントでログイン</p>
+        </div>
+    )
+}
+
+export default SignIn
