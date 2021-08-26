3de8203de29872fb87732d908358872f2a777f56
takapiro99 <55702777+takapiro99@users.noreply.github.com>
2021-08-27 01:45:56 +0900

    twitterでログインできるようになりました

--- frontend/pages/auth/signin.tsx
+++ frontend/pages/auth/signin.tsx
@@ -1,11 +1,29 @@
 import styles from './Signin.module.scss'
+import { useContext } from 'react'
+import { AuthContext } from '../../src/lib/AuthProvider'
+
+/* eslint @next/next/no-img-element:0 */
+
+// TODO: google のアイコンとかの利用規約とか調べる
 
 const SignIn = () => {
+    const context = useContext(AuthContext)
+
     return (
         <div>
-            <p className={styles.title}>BOOKTREEにログインする</p>
-            <p className={styles.Twitter}>Twitterアカウントでログイン</p>
-            <p className={styles.Google}>Googleアカウントでログイン</p>
+            <h1 className={styles.title}>BOOKTREEにログインする</h1>
+            <p className={`${styles.Twitter} ${styles.icon}`} onClick={context.twitterLogin}>
+                <img
+                    src="/twitter-blue.svg"
+                    alt="twitter icon"
+                    height={35}
+                    style={{ display: 'inline-block', marginRight: '15px' }}
+                />
+                twitter で続ける
+            </p>
+            <p className={`${styles.Google} ${styles.icon}`} onClick={context.googleLogin}>
+                Googleアカウントでログイン
+            </p>
         </div>
     )
 }
