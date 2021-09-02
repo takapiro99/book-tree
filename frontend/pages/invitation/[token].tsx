eaa496fdca0537e518ac0e805a5812458df811fc
takapiro99 <55702777+takapiro99@users.noreply.github.com>
2021-09-02 21:44:37 +0900

    新規投稿の suggestion 表示まで

--- frontend/pages/invitation/[token].tsx
+++ frontend/pages/invitation/[token].tsx
@@ -4,9 +4,9 @@ import { useEffect, useState } from 'react'
 import GuardedRoute from '../../components/auth/GuardedRoute'
 import SignInWithTwitterOrGoogle from '../../components/auth/SignInWithTwitterOrGoogle'
 import NewPost from '../../components/NewPost'
-import { checkInvitation } from '../../lib/api'
+import { checkInvitation, getUserInfo } from '../../lib/api'
 import { AuthContext } from '../../lib/AuthProvider'
-import { Invitation } from '../../lib/types'
+import { Invitation, UserInfo } from '../../lib/types'
 import styles from '../../styles/invitation.module.scss'
 
 /* eslint @next/next/no-img-element:0 */
@@ -16,8 +16,9 @@ import styles from '../../styles/invitation.module.scss'
 const InviteReview = () => {
     const [loadingInvitation, setLoadingInvitation] = useState<boolean>(true)
     const [invitation, setInvitation] = useState<Invitation | undefined>(undefined)
+    const [inviter, setInviter] = useState<UserInfo | undefined>(undefined)
     const router = useRouter()
-    const { currentUser, userInfo } = useContext(AuthContext)
+    const { currentUser } = useContext(AuthContext)
     const { token } = router.query
 
     useEffect(() => {
@@ -40,12 +41,22 @@ const InviteReview = () => {
         }
     }, [token]) // eslint-disable-line
 
+    useEffect(() => {
+        if (invitation) {
+            getUserInfo(invitation.from).then((user) => {
+                if (user) {
+                    setInviter(user)
+                }
+            })
+        }
+    }, [invitation])
+
     if (loadingInvitation) {
         return <p style={{ textAlign: 'center' }}>loading...</p>
     }
 
     return (
-        <div className={styles.nominateBlock}>
+        <div className={`${styles.nominateBlock} container`}>
             <div className={styles.nominateReasonWrapper}>
                 <h1 className={styles.nominateBlock__reason}>
                     デザインがすごいあなたに
@@ -57,7 +68,7 @@ const InviteReview = () => {
             </div>
             <div className={styles.nominateFromWho}>
                 <div className={styles.icon}>
-                    <img src="/images/foxIcon.png" alt="icon" />
+                    <img src={inviter?.profileImage} alt="icon" />
                 </div>
                 <img src="/images/greencloud.png" alt="cloud" className={styles.greenCloud} />
             </div>
